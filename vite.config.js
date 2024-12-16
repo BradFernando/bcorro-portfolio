e480011f02d12a9fc45ad8eb44c288115c986ff5
BradFernando <bfcorro@espe.edu.ec>
2024-12-16 01:25:36 -0500

    Second Commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
+  base: '/', // Asegura que la ruta base esté bien definida
+  build: {
+    outDir: 'dist', // Asegura que el build se guarde en 'dist'
+    assetsDir: 'assets', // Carpeta para recursos estáticos
   },
 });
